1336b6f6e91ed6465429c23e84205ae5c81c9173
Yuqin Chen <yuqinchen@Yuqins-MacBook-Air.local>
2016-12-20 12:26:08 -0600

    how to upload

---
+++ 00HowToUpload.py
@@ -0,0 +1,13 @@
+'''
+how to upload to github
+
+pwd: path of current directory
+cd  ...: change directory
+ls : list
+
+once in the hackerrank_problems directory
+git add  ...
+git status
+git commit -m "..."
+git push -u origin master
+'''
